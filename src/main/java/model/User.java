f18eeb3ac2d049a721dd0e42b6f2607d87df8ecc
Ivan Teleshov <ivant66@yandex.ru>
2016-08-25 21:38:49 +0300

    Created package "model" and "repository". Also created class "User" that defined as Entity

---
+++ src/main/java/model/User.java
@@ -0,0 +1,83 @@
+package model;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+/**
+ * Created by Jager on 25.08.2016.
+ */
+@Entity
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "seq", sequenceName = "seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    private Integer id;
+
+    @NotEmpty
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @NotEmpty
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column(name = "position")
+    private String position;
+
+    @Column
+    private LocalDate birthDay;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+}
