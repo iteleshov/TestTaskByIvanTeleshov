a8b20914ee87e1e10e0abf670815e00621d5f5de
Ivan Teleshov <ivant66@yandex.ru>
2016-08-27 22:44:31 +0300

    Changed UserRepository

--- src/main/java/com/teleshovivan/repository/UserRepository.java
+++ src/main/java/com/teleshovivan/repository/UserRepository.java
@@ -1,16 +1,17 @@
 package com.teleshovivan.repository;
 
 import com.teleshovivan.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Created by Jager on 25.08.2016.
  */
-@Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     @Override
     List<User> findAll();
 }
