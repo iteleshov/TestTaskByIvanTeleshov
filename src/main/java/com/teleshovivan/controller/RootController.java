312797cd93f7ac18d0c68530bce9ab99ef559e64
Ivan Teleshov <ivant66@yandex.ru>
2016-08-29 00:46:14 +0300

    Changed and added dependencies in pom.xml

--- src/main/java/com/teleshovivan/controller/RootController.java
+++ src/main/java/com/teleshovivan/controller/RootController.java
@@ -2,11 +2,13 @@ package com.teleshovivan.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-//@Controller
+@Controller
+@RequestMapping("/")
 public class RootController {
 
-    @RequestMapping("/")
+    @RequestMapping(method = RequestMethod.GET)
     public String index() {
         return "index.html";
     }
