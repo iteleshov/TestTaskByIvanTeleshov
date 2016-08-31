b90e25660590c7f3211dc33b650118bd1ca44f9c
egervan <ivant66@yandex.ru>
2016-08-31 09:48:55 +0300

    Fixed problem with choosing date using datetimepicker

--- src/main/resources/static/js/date.js
+++ src/main/resources/static/js/date.js
@@ -7,12 +7,7 @@ function datepicker(dateField) {
         timepicker: false,
         format: 'Y-m-d',
         lang: 'ru',
-        formatDate: 'Y-m-d',
-        onShow: function () {
-            this.setOptions({
-                minDate: dateField.val() ? dateField.val() : false
-            })
-        }
+        formatDate: 'Y-m-d'
     })
 }
 
