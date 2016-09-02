9e6752626fb463ec7cd36f7dea49b9070e86cfd3
egervan <ivant66@yandex.ru>
2016-09-02 11:49:48 +0300

    Removed unused imports

--- src/main/java/com/teleshovivan/repository/UserRepository.java
+++ src/main/java/com/teleshovivan/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.teleshovivan.repository;
 
 import com.teleshovivan.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
--- src/main/java/com/teleshovivan/util/LocalDateConverter.java
+++ src/main/java/com/teleshovivan/util/LocalDateConverter.java
@@ -1,9 +1,6 @@
 package com.teleshovivan.util;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.sql.Date;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
